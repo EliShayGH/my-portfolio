5d388acffb05aa3eff65b103f887a3efd724b454
Eli Shay <El_mn123@hotmail.com>
2018-06-22 00:03:28 +0300

    added: hover src change for projects

---
+++ js/designs.js
@@ -0,0 +1,17 @@
+function hover1(element) {
+  element.setAttribute('src', 'images/animal-card-hover.gif');
+}
+
+function unhover1(element) {
+  element.setAttribute('src', 'images/animal-card.png');
+}
+
+function hover2(element) {
+  element.setAttribute('src', 'images/pixel-art-maker-hover.png');
+}
+
+function unhover2(element) {
+  element.setAttribute('src', 'images/pixel-art-maker.png');
+}
+
+d
